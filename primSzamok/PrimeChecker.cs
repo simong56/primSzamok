c8842d87a76d1f7bf653e0feaeede3a92b902bf1
simong1 <simong1@kkszki.hu>
2025-12-10 11:59:30 +0100

    Add project files.

---
+++ primSzamok/PrimeChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primSzamok
+{
+    internal class PrimeChecker
+    {
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
